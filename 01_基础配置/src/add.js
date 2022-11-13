bd869ac7d798671af03dd09b889e59f55ec78ef0
wanglish <739100735@qq.com>
2022-11-13 17:18:52 +0800

    01-webpack的基础配置

---
+++ 01_基础配置/src/add.js
@@ -0,0 +1,3 @@
+export default function addFun (...args) {
+  return args.reduce((sum, current) => sum + current, 0)
+}
---
+++ 01_基础配置/src/main.js
@@ -0,0 +1,5 @@
+import addFun from './add'
+import alertFun from './alert'
+
+console.log(addFun(1, 2, 3, 4));;
+alertFun(55);
