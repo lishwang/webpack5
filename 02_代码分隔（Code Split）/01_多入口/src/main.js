1c2da367607b1223a57b98a6baf3306d299471a7
wanglish <739100735@qq.com>
2022-11-24 22:25:04 +0800

    笔记补充

--- 02_代码分隔（Code Split）/01_多入口/src/main.js
+++ 02_代码分隔（Code Split）/01_多入口/src/main.js
@@ -7,7 +7,7 @@ console.log(sum(1, 2, 3));
 document.getElementById('loadBtn').onclick = function () {
   // console.log(load(3, 3)); // 直接触发，打包时直接将结果打包进main中
 
-  // 按需加载
+  // 按需加载，import 动态导入，会将动态导入的文件代码分隔（打包时拆分成单独的文件），在需要使用时自动加载该文件，并执行相应功能；
   import('./afterLoad').then(res => {
     console.log('按需加载文件成功');
     console.log(res.load_mul(3, 3));
--- 02_代码分隔（Code Split）/01_多入口/webpack.config.js
+++ 02_代码分隔（Code Split）/01_多入口/webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  // 单入口
+  // entry: './src/main.js',
+
   // 多入口，有几个入口就一定会有几个及以上的文件输出，至于会不会有多个以上文件的输出，需在optimization中配置splitChunks；
   entry: {
     name_app: './src/app.js',
@@ -29,11 +32,11 @@ module.exports = {
   optimization: {
     // 代码分割配置
     splitChunks: {
-      chunks: "all", // 对所有模块都进行分割
+      chunks: "all", // 对所有模块都进行分割，必填配置；如果为单入口，一般情况下只写这一个配置即可满足大部分情况；
       // 以下是默认值
       // minSize: 20000, // 分割代码最小的大小，文件体积小于设定值时不会被分隔单独打包；
       // minRemainingSize: 0, // 类似于minSize，最后确保提取的文件大小不能为0；
-      // minChunks: 1, // 至少被引用的次数，满足条件才会代码分割
+      // minChunks: 1, // 被打包输出的文件引用的最少次数，满足条件才会代码分割（这里打包输出的文件只会在多入口时，且这些入口文件中引入某文件的次数大于等于设定值时，会将引入的该文件打包成单独的模块）
       // maxAsyncRequests: 30, // 按需加载时并行加载的文件的最大数量，超过设置的30个文件后就不会再抽取成模块额外打包了，虽然把文件提取出来可以做到每个文件体积都减小，但是请求数量会变多，对服务器压力增大，因此最好设置临界值；
       // maxInitialRequests: 30, // 入口js文件最大并行请求数量；
       // enforceSizeThreshold: 50000, // 超过50kb一定会单独打包（此时会忽略minRemainingSize、maxAsyncRequests、maxInitialRequests）
@@ -44,11 +47,12 @@ module.exports = {
       //     reuseExistingChunk: true, // 如果当前 chunk 包含已从主 bundle 中拆分出的模块，则它将被重用，而不是生成新的模块
       //   },
       //   default: { // 其他没有写的配置会使用上面的默认值
-      //     minChunks: 2, // 至少引用两次，才会被打包到 default 这个组中，这里的minChunks权重更大，会覆盖掉上面的默认配置；
+      //     minChunks: 2, // 至少有两个入口文件，且入口文件中至少有两个引用了该文件，才会被打包到 default 这个组中，这里的minChunks权重更大，会覆盖掉上面的默认配置；
       //     priority: -20, // （-20 < -10 因此 default 权重 小于 defaultVendors，打包时会有限匹配defaultVendors的规则）；
       //     reuseExistingChunk: true,
       //   },
       // },
+
       // 修改配置，以下这个配置组仅为当前文件有用，适配当前文件做代码分隔
       cacheGroups: {
         // 组，哪些模块要打包到一个组
