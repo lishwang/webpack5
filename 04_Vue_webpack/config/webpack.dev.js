d32755b37250513dd2197d7739073154e9d3e9f2
wanglish <739100735@qq.com>
2022-12-11 21:44:04 +0800

    自己搭建vue脚手架配置

--- 04_Vue_webpack/config/webpack.dev.js
+++ 04_Vue_webpack/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
   entry: './src/main.js',
@@ -14,106 +15,115 @@ module.exports = {
   module: {
     rules: [
       {
-        oneOf: [
+        test: /\.css$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
           {
-            test: /\.css$/,
-            use: [
-              "style-loader",
-              "css-loader",
-              {
-                loader: "postcss-loader",
-                options: {
-                  postcssOptions: {
-                    plugins: [
-                      [
-                        "postcss-preset-env",
-                      ],
-                    ],
-                  },
-                },
-              },
-            ],
-          },
-          {
-            test: /\.less$/,
-            use: [
-              "style-loader",
-              "css-loader",
-              {
-                loader: "postcss-loader",
-                options: {
-                  plugins: [
-                    [
-                      "postcss-preset-env",
-                    ],
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
                   ],
-                },
+                ],
               },
-              "less-loader",
-            ],
+            },
           },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
           {
-            test: /\.s[ac]ss$/,
-            use: [
-              "style-loader",
-              "css-loader",
-              {
-                loader: "postcss-loader",
-                options: {
-                  plugins: [
-                    [
-                      "postcss-preset-env",
-                    ],
-                  ],
-                },
-              },
-            ],
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                [
+                  "postcss-preset-env",
+                ],
+              ],
+            },
           },
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
           {
-            test: /\.styl$/,
-            use: [
-              "style-loader",
-              "css-loader",
-              {
-                loader: "postcss-loader",
-                options: {
-                  plugins: [
-                    [
-                      "postcss-preset-env",
-                    ],
-                  ],
-                },
-              },
-            ],
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                [
+                  "postcss-preset-env",
+                ],
+              ],
+            },
           },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
           {
-            test: /\.(png|jpe?g|gif|svg)$/,
-            type: 'asset',
-            parser: {
-              dataUrlCondition: {
-                maxSize: 4 * 1024 // 4kb
-              }
-            }
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                [
+                  "postcss-preset-env",
+                ],
+              ],
+            },
           },
-          {
-            test: /\.(ttf|woff2?|mp3|mp4)$/,
-            type: "asset/resource",
-          },
-          {
-            test: /\.js$/,
-            exclude: /node_modules[\\/]/,
-            // include: path.resolve(__dirname, "../src"), // 指定处理范围
-            use: {
-              loader: 'babel-loader',
-              options: {
-                cacheDirectory: true,
-                cacheCompression: false,
-                presets: ['@babel/preset-env']
-              }
-            }
+          "stylus-loader"
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024 // 4kb
           }
-        ]
-      }
+        }
+      },
+      {
+        test: /\.(ttf|woff2?|mp3|mp4)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules[\\/]/,
+        // include: path.resolve(__dirname, "../src"), // 指定处理范围
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            cacheCompression: false,
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader", // 内部会给vue文件注入HMR功能代码
+        options: {
+          // 开启缓存
+          cacheDirectory: path.resolve(
+            __dirname,
+            "node_modules/.cache/vue-loader"
+          ),
+        },
+      },
     ]
   },
   plugins: [
@@ -125,7 +135,14 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "../public/index.html"),
-    })
+    }),
+    // vue-loader 需要引用的插件
+    new VueLoaderPlugin(),
   ],
   mode: "development",
+  // webpack解析模块时加载的选项
+  resolve: {
+    // 模块引入时，不写后缀名时自动补全文件扩展名
+    extensions: [".vue", ".js", ".json"],
+  },
 }
---
+++ 04_Vue_webpack/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="title">App</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.title {
+  color: pink;
+}
+</style>
